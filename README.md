a009711fe93a51d14a6997b21a61620ccb118597
Vinicius <viniciusalvescosta07@gmail.com>
2025-07-02 21:33:14 -0300

    adicionado README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Sistema_Caixa_Eletronico
+
+## Problema
+Implemente um sistema de gerenciamento de um caixa eletrônico com base nos **pilares do pensamento computacional.** A solução deve:
+- Receber a configuração inicial de cédulas disponíveis no caixa eletrônico (valores e quantidades).
+- Entrar em modo de operação, permitindo **múltiplos saques**.
+- A cada saque solicitado, tentar entregar o valor exato com o menor número de cédulas possível.
+- Caso não haja cédulas suficientes para atender ao valor solicitado, exibir mensagem de erro **(“Valor indisponível para saque”).**
+- **O CAIXA NÃO DEVE ENTREGAR TROCO NEM VALORES PARCIAIS — OU ENTREGA O VALOR EXATO OU NÃO ENTREGA**
+
+```
+Exemplo de Entrada Inicial:
+    > Cédulas 100, 50, 20, 10
+    > Quantidades 2, 4, 5, 10
+Exemplos de Saques:
+    > Saque de 180 → entrega 2x100, 1x50, 1x20, 1x10.
+    > Saque de 70 → entrega 1x50, 1x20
+    > Saque de 250 → não há cédulas suficientes
+```
+
+##  Entregas:
+Tipo        | Status
+:---:         | :---:
+Fluxograma  | ✅
+Programa C  | ✅
+Mips-32     | ⏳ / ⚙️
+Python      | ⏳ / ⚙️
--- main.c
+++ main.c
@@ -1,24 +1,7 @@
 /****************************************************************************
 Título Desenvolvimento de Sistema Simplificado para Caixa Eletrônico
-Problema
-- Implemente um sistema de gerenciamento de um caixa eletrônico com base nos pilares do pensamento computacional. A solução deve:
-- Receber a configuração inicial de cédulas disponíveis no caixa eletrônico (valores e quantidades).
-- Entrar em modo de operação, permitindo múltiplos saques.
-- A cada saque solicitado, tentar entregar o valor exato com o menor número de cédulas possível.
-- Caso não haja cédulas suficientes para atender ao valor solicitado, exibir mensagem de erro (“Valor indisponível para saque”).
-- O caixa não deve entregar troco nem valores parciais — ou entrega o valor exato ou não entrega.
-Exemplo de Entrada Inicial
-> Cédulas 100, 50, 20, 10
-> Quantidades 2, 4, 5, 10
-Exemplos de Saques
-> Saque de 180 → entrega 2x100, 1x50, 1x20, 1x10.
-> Saque de 70 → entrega 1x50, 1x20
-> Saque de 250 → não há cédulas suficientes
-Entregas
-- Fluxograma
-- Programa C
--AUTOR-> VINICIUS ALVES COSTA
--EMAIL-> viniciusac.aluno@unipampa.edu.br
+-AUTOR- VINICIUS ALVES COSTA
+-EMAIL- viniciusac.aluno@unipampa.edu.br
 *******************************************************************************/
 #include <stdio.h>
 
